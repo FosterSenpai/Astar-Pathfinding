20c22442a64b30b618b00073342df2cc820770ee
FosterRae <fosterjameskiatarae@gmail.com>
2024-11-14 12:04:04 +1300

    Add VS solution, project files, and update .gitignore
    - Updated .gitignore to ignore Visual Studio.
    
    - Added header file `c_dungeon_map.h` with class definition and metadata.

---
+++ Project1/c_dungeon_map.h
@@ -0,0 +1,19 @@
+﻿// /***********************************************************************
+// Bachelor of Software Engineering
+// Media Design School
+// Auckland
+// New Zealand
+// (c) 2024 Media Design School
+// File Name : c_dungeon_map.h
+// Description : Methods for loading, verifying, displaying and saving the dungeon map.
+// Author : Foster Rae
+// Mail : Foster.Rae@mds.ac.nz
+// ************************************************************************/
+
+#pragma once
+
+class c_dungeon_map
+{
+public:
+	
+};
